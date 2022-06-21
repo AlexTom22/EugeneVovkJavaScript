e59406b2666fe73ca8ab61bf7b1c00ddf9529a4d
Eugene Vovk <72972961+EugeneVovk@users.noreply.github.com>
2022-06-21 03:16:03 +0300

    Update hw1.0.js

--- JS.HW_1/Anatoly_Karpovich/hw1.0.js
+++ JS.HW_1/Anatoly_Karpovich/hw1.0.js
@@ -29,7 +29,7 @@ const age3 = 60;
 if (age < age2) {
     console.log(`You don't have access cause your age is ${age}. It's less then `);
 } else if (age >= age2 && age <= age3) {
-    alert('Welcome!');
+    console.log('Welcome!');
 } else if (age > age3) {
     console.log('Keep calm and look Culture channel');
 } else {
