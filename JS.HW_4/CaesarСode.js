d9ac16b42f7bec14bc300fa22f5191c79cb6c399
Eugene Vovk <eugenevovk@gmail.com>
2022-06-27 04:34:00 +0300

    add alphabet checking

--- JS.HW_4/CaesarСode.js
+++ JS.HW_4/CaesarСode.js
@@ -17,7 +17,7 @@
  и дважды смещали назад (декодировали):)
  */
 
-function caesarCode(word, code) {
+ function caesarCode(word, code) {
     let result = '';
     for (let i = 0; i < code.length; i++) {
         code[i] = code[i].match(/\d/);
@@ -25,23 +25,43 @@ function caesarCode(word, code) {
             if (Number(code[i]) === 1) {
                 result = '';
                 for (let el of word) {
-                    result += String.fromCharCode(el.charCodeAt() + 1);
+                    let temp = String.fromCharCode(el.charCodeAt() + 1);
+                    if (isLetter(temp)) {
+                        result += temp;
+                    } else {
+                        result += el;
+                    }
                 }
             } else if (Number(code[i]) === 0) {
                 result = '';
                 for (let el of word) {
-                    result += String.fromCharCode(el.charCodeAt() - 1);
+                    let temp = String.fromCharCode(el.charCodeAt() - 1);
+                    if (isLetter(temp)) {
+                        result += temp;
+                    } else {
+                        result += el;
+                    }
                 }
             }
             word = result;
         }
     }
+    function isLetter(char) {
+        return char.match(/^[a-z]+$/i);
+    }
     return result;
 }
 
 // test
-console.log(caesarCode("Hello", "C0"));
-console.log(caesarCode("Hello", "C1"));
-console.log(caesarCode("Hello", "C1-C0"));
-console.log(caesarCode("Hello", "C1-C0-C1"));
-console.log(caesarCode("Hello", "C1-C0-C1-C0"));
+console.log('1', caesarCode("Hello", "C0"));
+console.log('2', caesarCode("Hello", "C1"));
+console.log('3', caesarCode("Hello", "C1-C0"));
+console.log('4', caesarCode("Hello", "C1-C0-C1"));
+console.log('5', caesarCode("Hello", "C1-C0-C1-C0"));
+console.log('6', caesarCode("apple", "C0"));
+console.log('7', caesarCode('fuzz', 'c1'));
+console.log('8', caesarCode("ZZZ", "C1"));
+console.log('9', caesarCode('aaa', 'C0'));
+console.log('10', caesarCode("zzz", "C1"));
+console.log('11', caesarCode("123", "C1-C0-C1-C0"));
+console.log('12', caesarCode("хэлоу", "C1-C0-C1-C0"));
